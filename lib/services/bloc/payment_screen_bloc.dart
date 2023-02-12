64cf3c6e3daed3515f9ac34b144c30c5a67eec51
usseiin <sirkenny.hk@gmail.com>
2023-02-12 04:42:24 +0100

    work with bloc as SM

--- lib/services/bloc/payment_screen_bloc.dart
+++ lib/services/bloc/payment_screen_bloc.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'package:bloc/bloc.dart';
+import 'package:chekout_app/data/models/model.dart';
 
 import 'package:chekout_app/data/repository/repository.dart';
+import 'package:chekout_app/models/pay_details.dart';
+import 'package:chekout_app/services/enum/payment_mode.dart';
+import 'package:chekout_app/services/logic/convert.dart';
 
 import '../enum/pricing_plan.dart';
 
@@ -15,15 +19,35 @@ class AppStateCubit extends Cubit<AppState> {
 
   void change(PricingPlan val) {
     if (val == PricingPlan.free) {
-      emit(state.copyWith(plan: PricingPlan.free));
+      emit(state.copyWith(
+          plan: PricingPlan.free,
+          rateSymbol: state.rateSymbol.copyWith(rate: val.price.toDouble())));
     }
 
     if (val == PricingPlan.basic) {
-      emit(state.copyWith(plan: PricingPlan.basic));
+      emit(state.copyWith(
+          plan: PricingPlan.basic,
+          rateSymbol: state.rateSymbol.copyWith(rate: val.price.toDouble())));
     }
     if (val == PricingPlan.premium) {
-      emit(state.copyWith(plan: PricingPlan.premium));
+      emit(state.copyWith(
+          plan: PricingPlan.premium,
+          rateSymbol: state.rateSymbol.copyWith(rate: val.price.toDouble())));
     }
+  }
+
+  Future<Details> getPaymebtDetails() async {
+    final result = await _apiClient.getPaymentDetail(
+      PayDetails(
+          productName: 'ChekOut plan',
+          description: state.plan.title,
+          localPrice: AmountCurrency(
+              amount: state.plan.price.toDouble(), currency: 'BTC'),
+          userData: UserData(id: "id", email: "email"),
+          pricingType: ""),
+    );
+
+    return result;
   }
 
   Future<void> _getRate(String query) async {
@@ -38,8 +62,15 @@ class AppStateCubit extends Cubit<AppState> {
     );
   }
 
+  double get convertedRate {
+    if (state.plan.price == state.rateSymbol.rate) {
+      return state.plan.price.toDouble();
+    }
+    return convert(state.plan.price, state.rateSymbol.rate);
+  }
+
   void updateScreen(String? query) {
-    if (state.selectedPaymentOption == "Cryptocurrency") {
+    if (state.selectedPaymentOption == PaymentMode.cryptocurrency) {
       _getRate(query!);
     } else {
       emit(state.copyWith(
@@ -53,30 +84,33 @@ class AppStateCubit extends Cubit<AppState> {
     emit(state.copyWith(isTandC: !state.isTandC));
   }
 
-  void updateSelectedPaymentOption(String val) {
-    emit(state.copyWith(selectedPaymentOption: val, isExpand: true));
+  void updateSelectedPaymentOption(PaymentMode val) {
+    emit(state.copyWith(selectedPaymentOption: val));
+  }
+
+  void resetSelected() {
+    emit(state.copyWith(selectedPaymentOption: null));
   }
 }
 
 class AppState {
   final PricingPlan plan;
-  final RateSymbol? rateSymbol;
+  final RateSymbol rateSymbol;
   final bool isTandC;
-  final String selectedPaymentOption;
+  final PaymentMode? selectedPaymentOption;
 
   const AppState({
     this.plan = PricingPlan.basic,
-    this.rateSymbol,
+    this.rateSymbol = const RateSymbol(symbol: "USD", rate: 0.0),
     this.isTandC = false,
-    this.selectedPaymentOption = "",
+    this.selectedPaymentOption,
   });
 
   AppState copyWith({
     PricingPlan? plan,
     RateSymbol? rateSymbol,
     bool? isTandC,
-    String? selectedPaymentOption,
-    bool? isExpand,
+    PaymentMode? selectedPaymentOption,
   }) {
     return AppState(
       plan: plan ?? this.plan,
